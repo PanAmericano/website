fc29b9c0222fa19ab4de15e70b1d6bd276adfd1b
Jonathan Guzman <jguzmarketing@gmail.com>
2017-03-14 19:52:58 -0700

    Delete settings.js

--- assets/js/settings.js
+++
@@ -1,6 +0,0 @@
- $(document).ready(function() {
-   $("#player").click(function(e) {
-   e.preventDefault();
-   $("#player").html('<iframe width="560" height="315" src="http://www.youtube.com/embed/_NXCY59PinE?rel=0&&controls=2&showinfo=0&autoplay=1" frameborder="0" allowfullscreen></iframe>');
-  });
-});
