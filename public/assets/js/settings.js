bf4a858a3edcbf56bd189f8db740e2521d1a8e57
Jonathan Guzman <jguzmarketing@gmail.com>
2017-03-15 17:50:57 -0700

    Font and Styling Updated

--- public/assets/js/settings.js
+++ public/assets/js/settings.js
@@ -1,6 +1,6 @@
  $(document).ready(function() {
    $("#player").click(function(e) {
    e.preventDefault();
-   $("#player").html('<iframe width="560" height="315" src="http://www.youtube.com/embed/_NXCY59PinE?rel=0&&controls=2&showinfo=0&autoplay=1" frameborder="0" allowfullscreen></iframe>');
+   $("#player").html('<iframe width="854" height="480" src="https://www.youtube.com/embed/?listType=playlist&list=PLwCbMb-X3S5J99BLtOoBWRA4w0bExrQd-&autoplay=1" frameborder="0" allowfullscreen></iframe>');
   });
 });
