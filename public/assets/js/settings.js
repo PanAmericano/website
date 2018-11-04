b7f8be070f74fb4cde9e79ad11f96ccb64f50b07
Jonathan Guzman <jguzmarketing@gmail.com>
2018-11-03 17:05:18 -0700

    Preperations before upgrading to React

--- public/assets/js/settings.js
+++ public/assets/js/settings.js
@@ -1,6 +1,6 @@
  $(document).ready(function() {
    $("#player").click(function(e) {
    e.preventDefault();
-   $("#player").html('<iframe width="853" height="480" src="https://www.youtube.com/embed/?listType=playlist&list=PLwCbMb-X3S5J99BLtOoBWRA4w0bExrQd-&autoplay=1" frameborder="0" allowfullscreen></iframe>');
+   $("#player").html('<iframe width="854" height="480" src="https://www.youtube.com/embed/?listType=playlist&list=PLwCbMb-X3S5J99BLtOoBWRA4w0bExrQd-&autoplay=1" frameborder="0" allowfullscreen></iframe>');
   });
 });
